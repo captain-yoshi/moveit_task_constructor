727d20fb17e16d7fe795d045df9f30d8ff051ac3
cpt-yoshi <cpt.yoshisaur@gmail.com>
2020-07-14 14:37:36 -0400

    Remove unnecessary files

--- demo/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bob/ws/ros/catkin_ws/src/moveit_task_constructor/demo
-# Build directory: /home/bob/ws/ros/catkin_ws/src/moveit_task_constructor/demo/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
